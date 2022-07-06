ebc0cfef6dbb0d0d039b120ce077e9f60049343c
Diego Perozo <diegodapt18@gmail.com>
2022-07-05 21:18:31 -0400

    removing the auth

--- task/views.py
+++ task/views.py
@@ -1,10 +1,11 @@
 from .models import Task
 from rest_framework import viewsets
-from rest_framework import permissions
+# from rest_framework import permissions
 from .serializers import TaskSerializer
+
 # Create your views here.
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
